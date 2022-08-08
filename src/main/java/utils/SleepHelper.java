ae0eb361dbfb0903b5fb1d4d2dc69c631b10f954
RhysNi <java_nsd@163.com>
2022-08-09 04:04:01 +0800

    Sleep工具类

---
+++ src/main/java/utils/SleepHelper.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Rhys.Ni
+ * @version 1.0
+ * @date 2022/8/9 12:16 上午
+ */
+public class SleepHelper {
+    public static void sleepSeconds(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
